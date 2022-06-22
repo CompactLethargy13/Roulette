80e751f15eb1bf16cab0acd66ebf4213ae2cc0ad
CompactLethargy13 <107340414+CompactLethargy13@users.noreply.github.com>
2022-06-22 21:59:16 +0800

    Update rouletteSingleFile.py

--- rouletteSingleFile.py
+++ rouletteSingleFile.py
@@ -1,4 +1,4 @@
-__version__ = "1.2.1"
+__version__ = "1.2.5" #build 10000
 '''
 ====================================================================================================================================================================================================
 IMPORTANT!!!!!!!! MAKE CHECK FOR GAMEMODE
@@ -13,15 +13,15 @@ add most if not all inside bets https://en.wikipedia.org/wiki/Roulette
 try best as i can to add outside bets
 ====================================================================================================================================================================================================
 '''
-#import
-import numbers
-import random
-import sys
-import os
-import logging
 from time import sleep
 import requests
+import os
 import urllib.request
+import numbers
+from turtle import clear
+import random
+import sys
+import logging
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 logging.basicConfig(filename = 'log.txt',level=logging.INFO)
@@ -35,12 +35,22 @@ firsts = [1,4,7,10,13,6,19,22,25,28,31,34]
 seconds = [2,5,8,11,14,17,20,23,26,29,32,35]
 thirds = [3,6,9,12,15,18,21,27,30,33,36]
 usrCheck = True
-gameMode = None
+gameMode = ''
 betIncrease = 0
 redOrBlack = None
 userCheck = "n"
+whichZero = ''
 logging.info("initialized")
 logging.info("checking version...")
+#===================================================================================================================================================================================================
+#def
+def clearscr():
+    if os.name == 'posix':
+        os.system("clear")
+    elif os.name == 'nt':
+        os.system('cls')
+    else:
+        print("Unknown Error: SystemUndefined\nProgram quit with exit code 4")
 def checkOdd(num):
     if (num%2) == 0:
         return False
@@ -124,36 +134,40 @@ def checkUpdates(__version__):
         input("ENTER to continue")
     os.system("cls")
     os.system("clear")
-def bettingMultiplier():
-    print("Betting on even or odd, or red or black    |   1: 1 return")
-    print("Betting on low or high range               |   1: 2 return")
-    print("Betting on a user-specified range          |   1: 3 return")
-    print("Betting on 0 or 00                         |   1:38 return")
-    input("ENTER to exit ")
 def plannedChanges():
     print("No planned changes currently")
     input("ENTER to exit ")
 class others:
     def aboutPage():
         pass
-    def helpPage():
-        quitOrNot = 'n'
-        whichPage = -1
-        while quitOrNot == 'n':
-            whichPage = int(input("Choose which page you want to go:\n1: Betting multiplier\n2: How to play\nInput here: (1/2) "))
-            while whichPage != 1 or whichPage != 2:
-                os.system('cls')
-                os.system('clear')
-                print("Please input 1 or 2")
-                whichPage = int(input("Choose which page you want to go:\n1: Betting multiplier\n2: Planned changes\n'q' to quit\nInput here: (1/2) "))
-            if whichPage == 1:
-                bettingMultiplier()
-            elif whichPage == 2:
-                plannedChanges()
-            elif whichPage == 'q':
-                quitOrNot = 'q'
-
-
+    def helpPage(question):
+        if help == '?':
+            quitOrNot = 'n'
+            whichPage = -1
+            while quitOrNot == 'n':
+                whichPage = input("Choose which page you want to go:\n1: Betting multiplier\n2: Planned changes\n'q' to quit\nInput here: (1/2) ")
+                while whichPage != '1' and whichPage != '2' and whichPage != 'q':
+                    #os.system('cls')
+                    #os.system('clear')
+                    if whichPage != 'repeat':
+                        print("Please input 1 or 2")
+                    whichPage = input("Choose which page you want to go:\n1: Betting multiplier\n2: Planned changes\n'q' to quit\nInput here: (1/2) ")
+                if whichPage == '1':
+                    #os.system('cls')
+                    #os.system('clear')
+                    print("Betting on even or odd, or red or black    |   1: 1 return")
+                    print("Betting on low or high range               |   1: 2 return")
+                    print("Betting on a user-specified range          |   1: 3 return")
+                    print("Betting on 0 or 00                         |   1:38 return")
+                    whichPage = 'repeat'
+                    input("ENTER to exit ")
+                    clearscr()
+                elif whichPage == '2':
+                    plannedChanges()
+                elif whichPage == 'q':
+                    clearscr()
+                    input("\033[1;37m  ______ _   _ _______ ______ _____    _              _             _   \n |  ____| \\ | |__   __|  ____|  __ \\  | |            | |           | |  \n | |__  |  \\| |  | |  | |__  | |__) | | |_ ___    ___| |_ __ _ _ __| |_ \n |  __| | . ` |  | |  |  __| |  _  /  | __/ _ \\  / __| __/ _` | '__| __|\n | |____| |\\  |  | |  | |____| | \\ \\  | || (_) | \\__ | || (_| | |  | |_ \n |______|_| \\_|  |_|  |______|_|  \\_\\  \\__\\___/  |___/\\__\\__,_|_|   \\__|")
+                    break
 #game starts
 
 #auto-update is disabled here for obvious reasons
@@ -164,10 +178,8 @@ logging.info("checking version done")
 os.system("cls")
 os.system("clear")
 help = input("\033[1;37m  ______ _   _ _______ ______ _____    _              _             _   \n |  ____| \\ | |__   __|  ____|  __ \\  | |            | |           | |  \n | |__  |  \\| |  | |  | |__  | |__) | | |_ ___    ___| |_ __ _ _ __| |_ \n |  __| | . ` |  | |  |  __| |  _  /  | __/ _ \\  / __| __/ _` | '__| __|\n | |____| |\\  |  | |  | |____| | \\ \\  | || (_) | \\__ | || (_| | |  | |_ \n |______|_| \\_|  |_|  |______|_|  \\_\\  \\__\\___/  |___/\\__\\__,_|_|   \\__|\nQuestion mark (?) for help page: ")
-if help == '?':
-    others.helpPage()
-os.system("cls")
-os.system("clear")
+others.helpPage(help)
+clearscr()
 #welcome text
 print("\033[1;31m __          __  _                            _                          _      _   _         _   _   _  \n \\ \\        / / | |                          | |                        | |    | | | |       | | | | | |\n  \\ \\  /\\  / ___| | ___ ___  _ __ ___   ___  | |_ ___    _ __ ___  _   _| | ___| |_| |_ ___  | | | | | |\n   \\ \\/  \\/ / _ | |/ __/ _ \\| '_ ` _ \\ / _ \\ | __/ _ \\  | '__/ _ \\| | | | |/ _ | __| __/ _ \\ | | | | | |\n    \\  /\\  |  __| | (_| (_) | | | | | |  __/ | || (_) | | | | (_) | |_| | |  __| |_| ||  __/ |_| |_| |_|\n     \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \\__\\___/  |_|  \\___/ \\__,_|_|\\___|\\__|\\__\\___| (_) (_) (_)\n")
 #credits
@@ -184,12 +196,22 @@ while credits > 0:
         print("\033[1;37mYou have",credits,"credits. ")
         #===========================================================================================================================================================================================
         #choice
-        gameMode = int(input("Do you want to bet on:\n1: Whether the number is odd or even\n2: What range the number will be in \n3: A specific range of numbers \n4: if the number will be 0 or 00\n5: If the number will be black or red\nInput what gamemode you want to play(1/2/3): "))
-        while gameMode not in range(1,5):
-            print("Please input a valid integer. ")
-            os.system('cls')
-            os.system('clear')
-            gameMode = int(input("Do you want to bet on:\n1: Whether the number is odd or even\n2: What range the number will be in \n3: A specific range of numbers \n4: if the number will be 0 or 00\n5: If the number will be black or red\nInput what gamemode you want to play(1/2/3): "))
+        while True:
+            gameMode = input("Do you want to bet on:\n1: Whether the number is odd or even\n2: What range the number will be in \n3: A specific range of numbers \n4: if the number will be 0 or 00\n5: If the number will be black or red\nInput what gamemode you want to play(1/2/3/4/5): ")
+            if gameMode == '':
+                clearscr()
+                continue
+            try:
+                gameMode = int(gameMode)
+            except (TypeError,ValueError):
+                clearscr()
+                print("Please input an integer between 1 and 6.")
+                continue
+            if gameMode not in range(1,6):
+                clearscr()
+                print("Please input a valid integer. ")
+                continue
+            break
         while True:
             try:
                 gameMode = int(gameMode)
@@ -220,7 +242,8 @@ while credits > 0:
         userCheck = input("Are you sure you want to continue? (y/n) ").lower()
     #==============================================================================================================================================================================================
     #generate number
-    number = random.randint(-1,36)
+    #number = random.randint(-1,36)
+    number = 0
     #==============================================================================================================================================================================================
     #check if guess is correct
     #odd or even
@@ -249,8 +272,9 @@ while credits > 0:
     #low or high
     elif gameMode == 2:
         while True:
-            lowOrHigh = input("Will the number be in: \n1: Range 1-12\n2: 13-24\n3: 25-36\n4: 1-19\n5: 20-36\n")
-            if lowOrHigh != '1' or lowOrHigh != '2' or lowOrHigh != '3' or lowOrHigh != '4' or lowOrHigh != '5':
+            lowOrHigh = input("Will the number be in: \n1: 1-12\n2: 13-24\n3: 25-36\n4: 1-19\n5: 20-36\n")
+            if lowOrHigh != '1' and lowOrHigh != '2' and lowOrHigh != '3' and lowOrHigh != '4' and lowOrHigh != '5':
+                print(type(lowOrHigh))
                 print("Please input an integer from 1 to 5")
             else:
                 lowOrHigh = int(lowOrHigh)
@@ -258,6 +282,7 @@ while credits > 0:
         #checkLow
         if lowOrHigh == 4 or lowOrHigh == 5:
             isLow = checkLow(number)
+            betIncrease = 2
             if lowOrHigh == isLow:
                 correct = True
             elif lowOrHigh != isLow:
@@ -268,6 +293,7 @@ while credits > 0:
                 sys.exit()
         elif lowOrHigh == 1 or lowOrHigh == 2 or lowOrHigh == 3:
             isRange = checkRange3(number)
+            betIncrease = 3
             if lowOrHigh == isRange:
                 correct = True
             elif lowOrHigh != isRange:
@@ -277,27 +303,36 @@ while credits > 0:
                 input("FATAL ERROR\nERROR CODE 1\nENTER to exit")
                 logging.error("checkRange3 failed")
                 sys.exit()
-        betIncrease = 2
     #what range
     elif gameMode == 3:
         while True:
             global usrRangeStart
             global usrRangeEnd
-            usrRangeStart = input("Input the start of the range: ")
+            input1 = input("Input the start of the range: ")
             try:
-                usrRangeStart = int(usrRangeStart)
+                input1 = int(input1)
             except (TypeError,ValueError):
                 print("Please input an integer.")
                 continue
             break
         while True:
-            usrRangeEnd = input("Input the end of the range: ")
+            input2 = input("Input the end of the range: ")
             try:
-                usrRangeEnd = int(usrRangeEnd)
+                input2 = int(input2)
             except (TypeError,ValueError):
                 print("Please input an integer.")
                 continue
-            break 
+            break
+        if (input2 in range(-2,2) and input1 in range(33,37)) or (input1 in range(-2,2) and input2 in range(33,37)):
+            clearscr()
+            print("Please input a valid range. ")
+            continue
+        if input1 > input2:
+            usrRangeEnd = input1
+            usrRangeStart = input2
+        else:
+            usrRangeEnd = input2
+            usrRangeStart = input1
         isInUsrRange = checkUserRange(usrRangeStart,usrRangeEnd,number)
         if isInUsrRange:
             correct = True
@@ -306,17 +341,19 @@ while credits > 0:
         betIncrease = 3
     #if its zero
     elif gameMode == 4:
-        whichZero = input("Will the number be 00 or 0: (0/00) ")
-        if number == 0 and whichZero == "0":
-            correct = True
-        elif number == -1 and whichZero == "00":
-            correct = True
-        else:
-            correct = False
+        whichZero = ''
+        while whichZero != '0' and whichZero != '00':
+            whichZero = input("Will the number be 00 or 0: (0/00) ")
+            if number == 0 and whichZero == "0":
+                correct = True
+            elif number == -1 and whichZero == "00":
+                correct = True
+            else:
+                correct = False
         betIncrease = 38
     #red or black
     elif gameMode == 5:
-        while redOrBlack != "red" or redOrBlack != "black":
+        while redOrBlack != "red" and redOrBlack != "black":
             print("reds: ",reds)
             print("blacks: ",blacks)
             redOrBlack = input("Will the number be a red or a black: (red/black) ").lower()
@@ -346,8 +383,9 @@ while credits > 0:
         input("Press ENTER to exit")
         sys.exit()
     input("ENTER to continue")
-    os.system("cls")
-    os.system("clear")
+    clearscr()
 
 print("You have",credits,"credits. ")
 print("\033[1;31m  _____          __  __ ______    ______      ________ _____  \n / ____|   /\\   |  \\/  |  ____|  / __ \\ \\    / |  ____|  __ \\ \n| |  __   /  \\  | \\  / | |__    | |  | \\ \\  / /| |__  | |__) |\n| | |_ | / /\\ \\ | |\\/| |  __|   | |  | |\\ \\/ / |  __| |  _  / \n| |__| |/ ____ \\| |  | | |____  | |__| | \\  /  | |____| | \\ \\ \n \\_____/_/    \\_|_|  |_|______|  \\____/   \\/   |______|_|  \\_\\")
+sys.exit()
+print("Error: ExitFailed\nThe code failed to exit. Please redownload the application. If the error continues, please fie an error report and wait for the reply. ")
